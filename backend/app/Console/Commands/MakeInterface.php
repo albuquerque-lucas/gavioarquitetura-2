0057f98706d7cd07c2bc7a0478a657cebb0569a1
albuquerque-lucas <lucaslpra@gmail.com>
2023-10-06 08:21:26 -0300

    Criando comandos de criacao de interface e repositorio

---
+++ backend/app/Console/Commands/MakeInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class MakeInterface extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:interface {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create new Interface';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $interfaceName = 'I' . $name;
+    
+        $directoryPath = app_path('Interfaces');
+        if (!File::exists($directoryPath)) {
+            File::makeDirectory($directoryPath, 0755, true);
+        }
+    
+        $content = "<?php\n\nnamespace App\Interfaces;\n\ninterface {$interfaceName}\n{\n    // Define methods for your interface\n}";
+    
+        $path = app_path("Interfaces/{$interfaceName}.php");
+    
+        File::put($path, $content);
+    
+        $this->info("Interface {$interfaceName} created successfully.");
+    }
+    
+}
