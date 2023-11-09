1251fe50ee12f40eda2d57f6bab49a50ebd0dc51
albuquerque-lucas <lucaslpra@gmail.com>
2023-11-09 16:41:21 -0300

    Refatorando tabela de edicao de projetos

--- frontend/src/components/admin/pages/ProjectShow/ProjectShowTable.jsx
+++ frontend/src/components/admin/pages/ProjectShow/ProjectShowTable.jsx
@@ -44,295 +44,66 @@ export default function ProjectShowTable() {
     }
   }
 
-  // const handleUpdate = (event, field) => {
-  //   event.preventDefault();
-  //   const data = await saveProject()
-  //   console.log('clicado!', field);
-  // }
-
   useEffect(() => {
     setEditedDetails(projectDetails);
   }, [projectDetails]);
 
+  const renderField = (label, value, field, placeholder) => (
+    <div className="row">
+      <div className="col-6 p-3 d-flex align-items-center info-edit-col">
+        <span className='col-3'>
+          {label}:&nbsp;
+        </span>
+        <span className='col-7 text-center'>
+          {value}
+        </span>
+        <button
+          className='btn btn-sm btn-dark mx-1 col-2'
+          onClick={(event) => handleEditMode(event, field)}
+        >
+          {updateSVG}
+        </button>
+      </div>
+      {editMode[field] ? (
+      <div className="col p-2 input-edit-col">
+        <div className="p-2 d-flex justify-content-center">
+          <input
+            type="text"
+            placeholder={placeholder}
+            value={editedDetails[field] || ''}
+            onChange={(e) => handleChange(field, e.target.value)}
+          />
+          <button
+            className='btn btn-sm btn-dark mx-1'
+            onClick={(e) => handleUpdate(e, field)}
+          >
+            {checkSVG}
+          </button>
+          <button
+            className='btn btn-sm btn-dark mx-1'
+            onClick={(event) => handleEditMode(event, field)}
+          >
+            {cancelSVG}
+          </button>
+        </div>
+      </div>
+    ) : null}
+    </div>
+  );
+    console.log(projectDetails);
   return (
-    <form action="">
-      <table className="table ps-table">
-        <tbody>
-          
-          <tr>
-            <td className="p-2 pst-head-cell">Image URL</td>
-            <td className='p-2 pst-body-cell'>
-              <span>{projectDetails.image_url}</span>
-              <button
-                className='btn btn-sm btn-dark mx-1'
-                onClick={(event) => handleEditMode(event, 'image_url')}
-              >
-                {updateSVG}
-              </button>
-            </td>
-            {editMode.image_url && (
-            <>
-              <td className='p-2'>
-                <input
-                  type="text"
-                  placeholder="Image URL"
-                  value={editedDetails.image_url || ''}
-                  onChange={(e) => handleChange('image_url', e.target.value)}
-                />
-            </td>
-            <td className='p-2'>
-              <button
-              className='btn btn-sm btn-dark mx-1'
-              onClick={(e) => handleUpdate(e, 'image_url')}
-              >
-                { checkSVG }
-              </button>
-              <button
-              className='btn btn-sm btn-dark mx-1'
-              onClick={(event) => handleEditMode(event, 'image_url')}
-              >
-                { cancelSVG }
-              </button>
-            </td>
-            </>
-          )}
-          </tr>
-          <tr>
-            <td className="p-2 pst-head-cell">Active Carousel</td>
-            <td className="p-2 pst-body-cell">
-              <span>{projectDetails.active_carousel}</span>
-              <button
-                className='btn btn-sm btn-dark mx-1'
-                onClick={(event) => handleEditMode(event, 'active_carousel')}
-              >
-                { updateSVG }
-              </button>
-            </td>
-            {editMode.active_carousel && (
-              <>
-                <td className="p-2">
-                  <input
-                    type="text"
-                    placeholder="Active Carousel"
-                    value={ editedDetails.active_carousel }
-                    onChange={(e) => handleChange('active_carousel', e.target.value)}
-                  />
-
-                </td>
-                <td>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(e) => handleUpdate(e, 'active_carousel')}
-                  >
-                    { checkSVG }
-                  </button>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(event) => handleEditMode(event, 'active_carousel')}
-                  >
-                    { cancelSVG }
-                  </button>
-                </td>
-              </>
-              )}
-          </tr>
-          <tr>
-            <td className="p-2 pst-head-cell">Name</td>
-            <td className="p-2 pst-body-cell">
-              <span>{projectDetails.name}</span>
-              <button
-                className='btn btn-sm btn-dark mx-1'
-                onClick={(event) => handleEditMode(event, 'name')}
-              >
-                { updateSVG }
-              </button>
-            </td>
-            {editMode.name && (
-              <>
-                <td className="p-2">
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    value={ editedDetails.name }
-                    onChange={(e) => handleChange('name', e.target.value)}
-                  />
-                </td>
-            <td>
-              <button
-              className='btn btn-sm btn-dark mx-1'
-              onClick={(e) => handleUpdate(e, 'name')}
-              >
-                { checkSVG }
-              </button>
-              <button
-              className='btn btn-sm btn-dark mx-1'
-              onClick={(event) => handleEditMode(event, 'name')}
-              >
-                { cancelSVG }
-              </button>
-            </td>
-              </>
-            )}
-          </tr>
-          <tr>
-            <td className="p-2 pst-head-cell">Category</td>
-            <td className="p-2 pst-body-cell">
-              <span>{projectDetails.category ? projectDetails.category.name : 'Categoria nao encontrada'}</span>
-              <button
-                className='btn btn-sm btn-dark mx-1'
-                onClick={(event) => handleEditMode(event, 'category')}
-              >
-                {updateSVG}
-              </button>
-            </td>
-            {editMode.category && (
-              <>
-                <td className="p-2">
-                  <input
-                    type="text"
-                    placeholder="Category"
-                    value={editedDetails.category ? editedDetails.category.name : 'Categoria nao encontrada'}
-                    onChange={(e) => handleChange('category', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(e) => handleUpdate(e, 'category')}
-                  >
-                    {checkSVG}
-                  </button>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(event) => handleEditMode(event, 'category')}
-                  >
-                    {cancelSVG}
-                  </button>
-                </td>
-              </>
-            )}
-
-          </tr>
-          <tr>
-            <td className="p-2 pst-head-cell">Area</td>
-            <td className="p-2 pst-body-cell">
-              <span>{projectDetails.area}</span>
-              <button
-                className='btn btn-sm btn-dark mx-1'
-                onClick={(event) => handleEditMode(event, 'area')}
-              >
-                {updateSVG}
-              </button>
-            </td>
-            {editMode.area && (
-              <>
-                <td className="p-2">
-                  <input
-                    type="text"
-                    placeholder="Area"
-                    value={editedDetails.area}
-                    onChange={(e) => handleChange('area', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(e) => handleUpdate(e, 'area')}
-                  >
-                    {checkSVG}
-                  </button>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(event) => handleEditMode(event, 'area')}
-                  >
-                    {cancelSVG}
-                  </button>
-                </td>
-              </>
-            )}
-
-          </tr>
-
-                <tr>
-                  <td className="p-2 pst-head-cell">Address</td>
-                  <td className="p-2 pst-body-cell">
-                    <span>{projectDetails.address}</span>
-              <button
-                className='btn btn-sm btn-dark mx-1'
-                onClick={(event) => handleEditMode(event, 'address')}
-              >
-                {updateSVG}
-              </button>
-                  </td>
-              {editMode.address && (
-                <>
-                  <td className="p-2">
-                    <input
-                      type="text"
-                      placeholder="Address"
-                      value={editedDetails.address}
-                      onChange={(e) => handleChange('address', e.target.value)}
-                    />
-                  </td>
-                  <td>
-                    <button
-                    className='btn btn-sm btn-dark mx-1'
-                    onClick={(e) => handleUpdate(e, 'address')}
-                    >
-                      {checkSVG}
-                    </button>
-                    <button
-                    className='btn btn-sm btn-dark mx-1'
-                    onClick={(event) => handleEditMode(event, 'address')}
-                    >
-                      {cancelSVG}
-                    </button>
-                  </td>
-                </>
-              )}
-
-                </tr>
-
-                <tr>
-            <td className="p-2 pst-head-cell">Description</td>
-            <td className="p-2 pst-body-cell">
-              <span>{projectDetails.description}</span>
-              <button
-                className='btn btn-sm btn-dark mx-1'
-                onClick={(event) => handleEditMode(event, 'description')}
-              >
-                {updateSVG}
-              </button>
-            </td>
-            {editMode.description && (
-              <>
-                <td className="p-2">
-                  <input
-                    type="text"
-                    placeholder="Description"
-                    value={editedDetails.description}
-                    onChange={(e) => handleChange('description', e.target.value)}
-                  />
-                </td>
-                <td>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(e) => handleUpdate(e, 'description')}
-                  >
-                    {checkSVG}
-                  </button>
-                  <button
-                  className='btn btn-sm btn-dark mx-1'
-                  onClick={(event) => handleEditMode(event, 'description')}
-                  >
-                    {cancelSVG}
-                  </button>
-                </td>
-              </>
-              )}
-          </tr>
-                </tbody>
-              </table>
-            </form>
+    <form action="" id='edit-project-form'>
+      <div className="container">
+        {renderField('Nome', projectDetails.name, 'name', 'Name')}
+        {renderField('Imagem', projectDetails.name, 'image_url', 'Image URL')}
+        {renderField('Area', projectDetails.area, 'area', 'Area')}
+        {renderField('Data', projectDetails.year, 'year', 'Data')}
+        {renderField('Categoria', projectDetails.category.name, 'category.name', 'Categoria')}
+        {renderField('Localizacao', projectDetails.address, 'address', 'Localizacao')}
+        {renderField('Descricao', projectDetails.description, 'description', 'Descricao')}
+        {renderField('Exibir na pagina inicial', projectDetails.active_carousel, 'active_carousel', 'Active Carousel')}
+        {/* Adicione mais campos conforme necessário */}
+      </div>
+    </form>
   );
 }
